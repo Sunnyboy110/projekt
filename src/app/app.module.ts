5ceebf91323f147ae3dd1338d699dc442b58c777
Jane2353 <Janevoetmann@gmail.com>
2023-03-23 13:08:42 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontEndComponent } from './front-end/front-end.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontEndComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
