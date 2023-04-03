51df7cd10cf13d9aed9f9ba95b8668b437c4ed9e
Sunnyboy110 <nitte1997@live.com>
2023-04-03 13:45:41 +0200

    soegefelt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontEndComponent } from './front-end/front-end.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SoegefeltComponent } from './soegefelt/soegefelt.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FrontEndComponent,
+    SoegefeltComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
