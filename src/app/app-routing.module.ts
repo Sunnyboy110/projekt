171ba6e6c5e427ccd97ac1e6a7fe3aeaf8404082
Adsfiklomnid <cbm.potech@gmail.com>
2023-04-03 13:18:56 +0200

    adminPage_tilbudsavis_plusOgMinus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { StoreComponent } from './pages/store/store.component';
+import { AdminSiteComponent } from './pages/admin-site/admin-site.component';
 
 const routes: Routes = [{
   path: 'home',
@@ -10,6 +11,10 @@ const routes: Routes = [{
 {
   path:'store',
   component: StoreComponent
+},
+{
+  path:'adminSite',
+  component: AdminSiteComponent
 },
 {
   path: '', redirectTo: 'home', pathMatch: 'full'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { StoreComponent } from './pages/store/store.component';
+import { AdminSiteComponent } from './pages/admin-site/admin-site.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     StoreComponent,
+    AdminSiteComponent,
   ],
   imports: [
     BrowserModule,
