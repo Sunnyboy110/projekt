1906ac351a971bfd8394cfd36181fc09972773b6
Sunnyboy110 <nitte1997@live.com>
2023-04-18 10:54:16 +0200

    tilføj/fjern

--- src/app/pages/store/store.component.ts
+++ src/app/pages/store/store.component.ts
@@ -13,16 +13,32 @@ export class StoreComponent {
 
   constructor(private dialogRef : MatDialog){}
   odre = [];
+  i = 1
 
   fremvisning(vare) {
     console.log(vare)
     this.odre = [...this.odre, vare]
+
   }
     openDialog(){
     this.dialogRef.open(PopUpComponent, {
       height: '75%',
       width: '45%'
     } );
+  }
+
+  plus(){
+    this.i++
+  }
+
+  fjern(){
+    if(this.i < 2){
+      delete this.odre
+    }
+      else {
+        this.i--
+      }
+      return this.i
   }
 }
 
